8247f461fb0e1efdf5cab5a3e20b522af7f86eaa
Mr-UmeshPathak <umesh979797@gmail.com>
2022-01-12 17:24:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css';
 
 function App() {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = () => {
+    axios
+      .get('https://pim.wforwoman.com/pim/pimresponse.php/?service=category&store=1&url_key=top-wear-kurtas&page=1&count=20&sort_by=&sort_dir=desc&filter=')
+      .then((res) => {
+        console.log(res.data.result);
+        setProducts(res.data.result.products);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Data is here</h1>
+      <div className='item-container'>
+        {
+          products.map((product) => (
+            <div className='card' key={product.id_product}>
+              <img src={product.image} alt='' />
+              <h3>{product.name}</h3>
+              <h2>$ {product.price}</h2>
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
